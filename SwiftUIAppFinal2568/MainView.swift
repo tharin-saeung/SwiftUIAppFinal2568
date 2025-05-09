0aa2bc97ac258ceadda65d314ee5aec0b67e29b7
John Doe <johndoe@email.com>
2025-05-09 18:56:19 +0700

    MainView

---
+++ SwiftUIAppFinal2568/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  SwiftUIAppFinal2568
+//
+//  Created by Nontakann nn on 9/5/2568 BE.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    MainView()
+}
