f5255d0bc64d1054def03e7a4da349fbf90b3679
Poowit somsak <poowitt.ps@gmail.com>
2025-05-12 23:54:07 +0700

    UI UP2

---
+++ SwiftUIAppFinal2568/DashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardView.swift
+//  SwiftUIAppFinal2568
+//
+//  Created by Poowit Somsak on 12/5/2568 BE.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardView()
+}
---
+++ SwiftUIAppFinal2568/ShopView.swift
@@ -0,0 +1,18 @@
+//
+//  ShopView.swift
+//  SwiftUIAppFinal2568
+//
+//  Created by Poowit Somsak on 12/5/2568 BE.
+//
+
+import SwiftUI
+
+struct ShopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShopView()
+}
