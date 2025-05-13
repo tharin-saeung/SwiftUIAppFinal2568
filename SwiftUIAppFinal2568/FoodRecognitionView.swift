0112b8d4837eb9c632723b980ea59bc134375c80
John Doe <johndoe@email.com>
2025-05-13 22:35:13 +0700

    FoodRecognition

---
+++ SwiftUIAppFinal2568/FoodRecognitionView.swift
@@ -0,0 +1,18 @@
+//
+//  FoodRecognitionView.swift
+//  SwiftUIAppFinal2568
+//
+//  Created by Nontakann nn on 13/5/2568 BE.
+//
+
+import SwiftUI
+
+struct FoodRecognitionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FoodRecognitionView()
+}
---
+++ SwiftUIAppFinal2568/FoodRecognitionViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  FoodRecognitionViewModel.swift
+//  SwiftUIAppFinal2568
+//
+//  Created by Nontakann nn on 13/5/2568 BE.
+//
+
+import SwiftUI
+
+struct FoodRecognitionViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FoodRecognitionViewModel()
+}
